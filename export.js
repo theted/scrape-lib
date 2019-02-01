8912f0e51a788b83a884d67eddc5a75ea48be2dd
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-02-01 09:21:15 +0100

    Export wrapper

---
+++ export.js
@@ -0,0 +1,6 @@
+const crawl = require('./lib/crawl')
+const send = require('./lib/send')
+const parse = require('./lib/parse')
+const scrape = require('./lib/scrape')
+
+module.exports = { crawl, send, parse, scrape }
