b0fcf1fc3ac1adccea395f92c285c5b529955964
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-02-03 19:51:02 +0100

    Add courtesy method wrapping a callback function around scrape method

--- export.js
+++ export.js
@@ -2,5 +2,6 @@ const crawl = require('./lib/crawl')
 const send = require('./lib/send')
 const parse = require('./lib/parse')
 const scrape = require('./lib/scrape')
+const process = require('./lib/process')
 
-module.exports = { crawl, send, parse, scrape }
+module.exports = { crawl, send, parse, scrape, process }
