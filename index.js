5fe319612a5b7deecd3654a7459abc94f4f834ad
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-02-04 11:32:08 +0100

    Add method for performing logins to remote sites

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const send = require('./lib/send')
 const parse = require('./lib/parse')
 const scrape = require('./lib/scrape')
 const process = require('./lib/process')
+const login = require('./lib/login')
 
-module.exports = { crawl, send, parse, scrape, process }
+module.exports = { crawl, send, parse, scrape, process, login }
