0a9c7d2b2222d20397d08f79ae537243aba9b7b5
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-02-04 15:39:03 +0100

    Add method for downloading remote files

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const parse = require('./lib/parse')
 const scrape = require('./lib/scrape')
 const process = require('./lib/process')
 const login = require('./lib/login')
+const download = require('./lib/download')
 const helpers = require('./lib/helpers')
 
-module.exports = { crawl, send, parse, scrape, process, login, helpers }
+module.exports = { crawl, send, parse, scrape, process, login, download, helpers }
