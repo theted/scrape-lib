18a2d4f3f475d44e2287eabf3b002b7c31b5f044
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-02-04 17:05:04 +0100

    Export links method

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const scrape = require('./lib/scrape')
 const process = require('./lib/process')
 const login = require('./lib/login')
 const download = require('./lib/download')
+const links = require('./lib/links')
 const helpers = require('./lib/helpers')
 
-module.exports = { crawl, send, parse, scrape, process, login, download, helpers }
+module.exports = { crawl, send, parse, scrape, process, login, download, links, helpers }
