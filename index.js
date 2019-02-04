bfbeb5c8b818933564930228870c9158afdfe747
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-02-04 11:32:44 +0100

    Export helpers

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const parse = require('./lib/parse')
 const scrape = require('./lib/scrape')
 const process = require('./lib/process')
 const login = require('./lib/login')
+const helpers = require('./lib/helpers')
 
-module.exports = { crawl, send, parse, scrape, process, login }
+module.exports = { crawl, send, parse, scrape, process, login, helpers }
